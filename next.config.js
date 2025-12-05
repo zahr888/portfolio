78c8b4b20ce1cca0e8bbcb49faabb2875af3e4da
zahr888 <zahr.hammemi@gmail.com>
2025-12-05 09:41:30 +0100

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: '/medzahreddinhammami',
+  assetPrefix: '/medzahreddinhammami/',
   trailingSlash: true,
   images: {
     unoptimized: true,
